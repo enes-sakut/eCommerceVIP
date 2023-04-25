c6ba00b5a86bdfb541ad1aaa870c99589fd78a0c
enes.sakut <enes.sakut@obss.com.tr>
2023-04-25 17:12:06 +0300

    clean session code

--- eCommerceVIP/App/Utils/Session.swift
+++ eCommerceVIP/App/Utils/Session.swift
@@ -9,19 +9,6 @@ import Foundation
 class Session {
     
     static let shared = Session()
-   
-//    var currentUsersStorage: UserDefaults? {
-//        get {
-//            guard let currentUserId = currentUser?.info?.userId else { return nil }
-//            return UserDefaults(suiteName: currentUserId)
-//        }
-//    }
-//
-//    private init() {
-//        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateTokenNotification(_:)), name: NSNotification.Name(rawValue: "didUpdateRefreshToken"), object: nil)
-//        currentUser = users?.first(where: { $0.active == true })
-//    }
-    
     var currentBasketCount: Int? {
         didSet {
             UserDefaults.standard.setValue(currentBasketCount, forKey: "currentBasketCount")
